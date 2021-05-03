c7433c88c38d94393c44caffe9f8c90cb2a50cdd
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2021-05-03 09:09:43 -0700

    Cambio de repositorio

---
+++ precache-manifest.2ce63a9c4f22ac19c3ef57e81425f29e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbdd0b2a0386cb03d983",
+    "url": "/css/app.56480879.css"
+  },
+  {
+    "revision": "fcb21c9772a915b3cd01",
+    "url": "/css/chunk-vendors.52eaf30c.css"
+  },
+  {
+    "revision": "27be105b22047b583c74d14abec8b195",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbdd0b2a0386cb03d983",
+    "url": "/js/app.63ac8ccd.js"
+  },
+  {
+    "revision": "fcb21c9772a915b3cd01",
+    "url": "/js/chunk-vendors.f28572a7.js"
+  },
+  {
+    "revision": "2291e54fad2b87963369111caf832780",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
